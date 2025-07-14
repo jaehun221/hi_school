83e4261b7eba161df0aa2b6aab75d5ab4e588fbf
jaehun221 <jhl2211768@gmail.com>
2025-07-14 19:19:56 +0900

    Initial commit of Spring Boot project

---
+++ src/main/java/com/hi_school/hi_school_api/HiSchoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.hi_school.hi_school_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiSchoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiSchoolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hi_school/hi_school_api/HiSchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hi_school.hi_school_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiSchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
