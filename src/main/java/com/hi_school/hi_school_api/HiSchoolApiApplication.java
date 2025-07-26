baed34f5f61e51898eeca261346760731ad4c6f6
jaehun221 <164811823+jaehun221@users.noreply.github.com>
2025-07-26 21:54:36 +0900

    Update HiSchoolApiApplication.java

--- src/main/java/com/hi_school/hi_school_api/HiSchoolApiApplication.java
+++ src/main/java/com/hi_school/hi_school_api/HiSchoolApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // 이 import 문을 추가합니다.
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.hi_school.hi_school_api.domain.user") // ⭐ 이 줄을 추가합니다.
+@EnableJpaRepositories(basePackages = "com.hi_school.hi_school_api.domain.user")
 public class HiSchoolApiApplication {
 
     public static void main(String[] args) {
